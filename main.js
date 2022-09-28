6c544a18d3e638610eb3b6986dcc2bd418cf4dc5
Belbut <andreferreirabelbut@gmail.com>
2022-09-28 20:19:40 +0100

    Add clear button js logic

--- main.js
+++ main.js
@@ -2,9 +2,46 @@ let rangeInput = document.getElementById("grid-size-range");
 let gridSizeLabel = document.getElementById("grid-size-label");
 gridSizeLabel.textContent = `Grid Size:${rangeInput.value}`;
 
-// TODO: Find a way to auto update this with out a EventListener
+const gridContent = document.getElementById("grid-content-container");
+
+const clearButton = document.getElementById("clear-button");
+const eraserButton = document.getElementById("eraser-button");
+
+
+// TODO: Find a way to auto update this with out a Event Listener
 rangeInput.addEventListener("click", () => {
     rangeInput = document.getElementById("grid-size-range");
     gridSizeLabel = document.getElementById("grid-size-label");
     gridSizeLabel.textContent = `Grid Size:${rangeInput.value}`;
+
+    clearGrid();
+    renderGrid(rangeInput.value);
 });
+
+clearButton.addEventListener("click", ()=>  {
+    clearGrid();
+    renderGrid();
+});
+
+
+
+function renderGrid(gridSize) {
+    for (let i = 1; i <= rangeInput.value; i++) {
+        const squareGrid = document.createElement("div");
+        squareGrid.setAttribute("class", "grid-box");
+        gridContent.append(squareGrid);
+    }
+
+
+}
+
+function clearGrid() {
+    const endOfLifeGridBox = document.getElementsByClassName("grid-box");
+    for(let i=endOfLifeGridBox.length; i>0;i--){
+        endOfLifeGridBox[i-1].remove();
+    }
+}
+    
+  
+
+renderGrid(rangeInput.value);
