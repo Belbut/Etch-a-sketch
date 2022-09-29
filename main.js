b37930e3111926f55cbea0a466723e8a975dc903
Belbut <andreferreirabelbut@gmail.com>
2022-09-29 12:02:22 +0100

    Initialize listeners and variables with onStart

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 let rangeInput = document.getElementById("grid-size-range");
 let gridSizeLabel = document.getElementById("grid-size-label");
-gridSizeLabel.textContent = `Grid Size:${rangeInput.value}`;
 
 const gridContent = document.getElementById("grid-content-container");
 
 const clearButton = document.getElementById("clear-button");
 const eraserButton = document.getElementById("eraser-button");
 
+onStart();
 
-// TODO: Find a way to auto update this with out a Event Listener
-rangeInput.addEventListener("click", () => {
-    rangeInput = document.getElementById("grid-size-range");
-    gridSizeLabel = document.getElementById("grid-size-label");
+function onStart() {
     gridSizeLabel.textContent = `Grid Size:${rangeInput.value}`;
-
-    clearGrid();
     renderGrid(rangeInput.value);
-});
 
-clearButton.addEventListener("click", ()=>  {
-    clearGrid();
-    renderGrid(rangeInput.value);
-});
+    // TODO: Find a way to auto update this with out a Event Listener
+    rangeInput.addEventListener("click", () => {
+        rangeInput = document.getElementById("grid-size-range");
+        gridSizeLabel = document.getElementById("grid-size-label");
+        gridSizeLabel.textContent = `Grid Size:${rangeInput.value}`;
 
+        clearGrid();
+        renderGrid(rangeInput.value);
+    });
 
+    clearButton.addEventListener("click", () => {
+        clearGrid();
+        renderGrid(rangeInput.value);
+    });
+}
 
 function renderGrid(gridSize) {
     for (let line = 1; line <= gridSize; line++) {
         const gridLine = document.createElement("div");
         gridLine.setAttribute("class", "grid-line");
         gridContent.append(gridLine);
-        for(let colum = 1; colum<=gridSize; colum++){
+        for (let colum = 1; colum <= gridSize; colum++) {
             const gridSquare = document.createElement("div");
-            gridSquare.setAttribute("class","grid-square");
+            gridSquare.setAttribute("class", "grid-square");
             gridLine.append(gridSquare);
         }
     }
 
 }
 
-
 function clearGrid() {
     const endOfLifeGridBox = document.getElementsByClassName("grid-line");
-    for(let i=endOfLifeGridBox.length; i>0;i--){
-        endOfLifeGridBox[i-1].remove();
+    for (let i = endOfLifeGridBox.length; i > 0; i--) {
+        endOfLifeGridBox[i - 1].remove();
     }
 }
-    
-  
 
-renderGrid(rangeInput.value);
+
